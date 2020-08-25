7ba646acb69cb0f0af103ae5c860c0f62f5cf8f6
Matiows <matewosg1216@gmail.com>
2020-08-25 09:15:46 +0300

    Revert "Initial commit"
    This reverts commit cee5e3ab4ad4f1a8ea7a414a32e0331be4bfd5bf.

--- akafay.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const akafay = express.Router()
-const Meri = require('./meri/sewMeri')
-
-akafay.get('/', Meri.bet)
-
-akafay.post('/register', Meri.mezgeb)
-
-module.exports = akafay
