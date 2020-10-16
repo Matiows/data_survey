2b4503c562dfcb437d17521607a4d3822d23d4f6
nedwerewolfbychoice <48416202+nedwerewolfbychoice@users.noreply.github.com>
2020-10-16 21:23:24 +0300

    LiveValidation (missing database check)

--- public/registrationForm.js
+++ public/registrationForm.js
@@ -7,6 +7,10 @@ export default class registrationForm {
         this.insertValidationElements()
         this.username = document.querySelector("#username")
         this.username.previousValue = ""
+        this.email = document.querySelector("#defaultRegisterFormEmail")
+        this.email.previousValue = ""
+        this.password = document.querySelector("#defaultRegisterFormPassword")
+        this.password.previousValue = ""
         this.events()
     }
     
@@ -15,6 +19,12 @@ export default class registrationForm {
         //TODO DISABLE FORM SUBMIT UNTIL ERRORS ARE CHECKED
         this.username.addEventListener('keyup', () => {
             this.isDifferent(this.username, this.usernameHandler)
+        })
+        this.email.addEventListener('keyup', () => {
+            this.isDifferent(this.email, this.emailHandler)
+        })
+        this.password.addEventListener('keyup', () => {
+            this.isDifferent(this.password, this.passwordHandler)
         })
     }
     isDifferent (el, handler) {
@@ -28,7 +38,6 @@ export default class registrationForm {
         this.usernameImmediate()
         clearTimeout(this.username.timer)
         this.username.timer = setTimeout(() => this.usernameDelayed (), 800);
-
     }
     usernameImmediate () {
         if (this.username.value!="" && !/^([a-zA-Z0-9]+)$/.test(this.username.value)){
@@ -36,6 +45,48 @@ export default class registrationForm {
         }
         if(!this.username.errors) {
             this.hideValidationErors(this.username)
+        }
+    }
+    emailHandler() {
+        this.email.errors = false
+        clearTimeout(this.email.timer)
+        this.email.timer = setTimeout(() => {
+            this.emailDelayed()
+        }, 800);
+
+    }
+
+    emailDelayed () {
+        if(!/^\S+@\S+$/.test(this.email.value)){
+            this.showValidationErrors(this.email, "you must provide a valid email address")
+        }
+        if (!this.email.errors) {
+            this.hideValidationErors(this.email)
+            //TODO CHECK IF EMAIL IS ALREADY TAKEN
+        }
+    }
+    passwordHandler () {
+        
+        this.password.errors = false
+        this.passwordImmediate()
+        clearTimeout(this.password.timer)
+        this.password.timer = setTimeout(() => {
+            this.passwordDelayed()
+        }, 800);
+
+
+    }
+    passwordDelayed () {
+        if (this.password.value.length<12) {
+            this.showValidationErrors(this.password, "password must be atleast 12 characters long")
+        }
+    }
+    passwordImmediate () {
+        if (this.password.value.length > 50) {
+            this.showValidationErrors(this.password, "password cannot be more than 50 characters")
+        }
+        if(!this.password.errors) {
+            this.hideValidationErors(this.password)
         }
     }
     
@@ -49,7 +100,7 @@ export default class registrationForm {
         el.errors = true
     }
     usernameDelayed () {
-        if(this.username.value<3) {
+        if(this.username.value.length<3) {
             this.showValidationErrors(this.username,"username must be more than 3 characters")
         }
         if (!this.username.errors) {
